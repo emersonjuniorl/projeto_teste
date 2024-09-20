110f7b0f840ab79511246a9b57a919feb8fb2d46
Emerson Junior Lourenço <emerson.lourenco@betha.com.br>
2024-09-20 16:12:41 -0300

    meu primeiro commit EJL

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Teste EJL - Jornada de Dados")
+
+#Variáveis
+
+idade = 37
+nome = "Emerson"
+
+#Print
+print(idade)
+
+#Operações
+nova_idade = idade + 1
+print(nova_idade)
+
+#Listas
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertida = lista.reverse()
